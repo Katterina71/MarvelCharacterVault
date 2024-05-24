1a998bac09d3ad1c2a24f71883704813d974b63e
Kate Iliu <beviatori@gmail.com>
2024-05-24 15:04:49 -0600

    Change http

--- react-characters-vault/src/api/marvelApi.js
+++ react-characters-vault/src/api/marvelApi.js
@@ -9,6 +9,6 @@ const privateKey = import.meta.env.VITE_API_KEY_PRIVATE;
 export function generateMarvelApiUrl(endpoint, limit = 100, offset = 0) {
   const ts = new Date().getTime().toString(); // Current timestamp
   const hash = CryptoJS.MD5(ts + privateKey + publicKey).toString();
-  const url = `http://gateway.marvel.com/v1/public/${endpoint}?ts=${ts}&apikey=${publicKey}&hash=${hash}&limit=${limit}&offset=${offset}`;
+  const url = `https://gateway.marvel.com/v1/public/${endpoint}?ts=${ts}&apikey=${publicKey}&hash=${hash}&limit=${limit}&offset=${offset}`;
   return url;
 }
