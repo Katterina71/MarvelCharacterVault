4c3dbdab5349b3a7e04e07cee030503376388a4a
Kate Iliu <beviatori@gmail.com>
2024-09-09 10:58:23 -0600

    Added sign

--- react-characters-vault/src/components/Footer.jsx
+++ react-characters-vault/src/components/Footer.jsx
@@ -1,11 +1,26 @@
 
 import '../style/Footer.css';
+import styled from 'styled-components';
+
+const StyledLink = styled.a`
+  color: grey;
+  text-decoration: none;
+  
+  &:hover {
+    color: #dc3545;
+  }
+`;
+
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>&copy; 2024 Marvel Characters App. All rights reserved.</p>
+      <p>
+          Concept and Development by 
+          <StyledLink href="https://beviatori.com/"> Ekaterina Iliushkina</StyledLink>
+      </p>
+      <p>&copy; 2024 Marvel Characters App. All rights reserved.</p>
       </div>
     </footer>
   );
